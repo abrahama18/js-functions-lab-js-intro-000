1da7a73808a723fe75dee37af750e63760abd45a
its-fluffy <abrahama37739@students.laalliance.org>
2016-11-01 18:04:28 +0000

    I am ready to send my files to github.

---
+++ index.js
@@ -0,0 +1,12 @@
+// write your code below!
+var happyHolidays = function (happyHolidays){
+  return "Happy holidays!";
+};
+
+var happyHolidaysTo = function (name){
+  return "Happy holidays, you!";
+};
+
+var happyHolidayTo = function (Independence){
+  return "Happy Independence Day, you!";
+};
